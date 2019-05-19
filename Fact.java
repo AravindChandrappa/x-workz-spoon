572dee62abb493b411293ab51c726ffe08c685e1
AravindChandrappa <46191186+AravindChandrappa@users.noreply.github.com>
2019-05-19 16:18:26 +0530

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -3,10 +3,10 @@ public class Fact
 {
     public static void main(String[] args)
     {
-        System.out.println("main method started")
+        System.out.println("main method started") 
         int fact=1;
         int num;
-
+//fdsbnsauydbjsdgiy saudbbkjh 
         Scanner scan=new Scanner(System.in);
         System.out.println("Enter the number to fact:");
         num=scan.nextInt();
